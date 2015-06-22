aaea3cfa29193c3f35c32987f79ad499c57b3531
Mike Vascelli <michele.vascelli@gmail.com>
2015-06-21 22:59:55 -0700

    added Requester tests

--- spec/lib/requester_spec.rb
+++ spec/lib/requester_spec.rb
@@ -9,13 +9,37 @@ describe Rubeetup::Requester do
       end
     end
 
-    context 'with invalid auth data' do
+    context 'with non-Hash-behaving auth data' do
       it 'raises InvalidAuthenticationError' do
         expect{Rubeetup::Requester.new('54545')}.to raise_error(Rubeetup::InvalidAuthenticationError)
       end
     end
 
+    context 'with Hash missing :api-key' do
+      it 'raises InvalidAuthenticationError' do
+        expect{Rubeetup::Requester.new(key: 'val')}.to raise_error(Rubeetup::InvalidAuthenticationError)
+      end
+    end
+
   end
 
+  describe 'handling requests' do
+    let(:agent) {Rubeetup::Requester.new(api_key: 'val')}
+    let(:request) {double(execute!: nil)}
+
+    before(:each) do
+      allow(agent.request_builder).to receive(:compose_request).and_return(request)
+    end
+
+    it 'delegates creation of requests' do
+      expect(agent.request_builder).to receive(:compose_request)
+      agent.get_events
+    end
+
+    it 'attempts to execute any user-sent request' do
+      expect(request).to receive(:execute!)
+      agent.get_events
+    end
+  end
 
 end
